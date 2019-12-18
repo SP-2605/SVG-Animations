41651f453b589835f33efe9d1801ebeef1d0c23b
Suriya Prasath.S <48484191+SP-2605@users.noreply.github.com>
2019-12-18 19:54:16 +0530

    Modified

--- Letter Animation/app.js
+++ Letter Animation/app.js
@@ -1,6 +1,15 @@
-const text = document.querySelectorAll("#logo path");
+console.log(document.getElementById('letter-1').getTotalLength());
 
-for(let i = 0; i<text.length; i++)
-{
-    console.log(text[i].getTotalLength());
-}
+console.log(document.getElementById('letter-2').getTotalLength());
+
+console.log(document.getElementById('letter-3').getTotalLength());
+
+console.log(document.getElementById('letter-4').getTotalLength());
+
+console.log(document.getElementById('letter-5').getTotalLength());
+
+console.log(document.getElementById('letter-6').getTotalLength());
+
+console.log(document.getElementById('letter-7').getTotalLength());
+
+console.log(document.getElementById('letter-8').getTotalLength());
